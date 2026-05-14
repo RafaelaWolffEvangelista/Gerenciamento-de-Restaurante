b5a9ef00736e7d91bb7163f2ca8650df866ce8de
rafaelaWolffEvangelista <rafaelawolffzimermannevangelis@gmail.com>
2026-05-13 22:08:09 -0300

    leituras
    leitura de ingrediente, categorias e produtos

--- restaurante/gerenciamento.cpp
+++ restaurante/gerenciamento.cpp
@@ -52,6 +52,47 @@ struct Consumo_Ingrediente {
     int codigo_ingrediente;
     int quant_necessaria;
 };
+void leituraCategoria (struct Categoria c[]) {
+    for (int i = 0; i < 3; i++) {
+        cout << "Categoria " << i+1 << ": ";
+        cout <<"Codigo: ";
+        cin >>c[i].codigo;
+        cout <<"Descricao: ";
+        cin >> c[i].descricao;
+    }
+}
+
+void leituraProduto (struct Produto p[]) {
+    for (int i = 0; i < 3; i++) {
+        cout <<"Produtos: " << i+1 << ": ";
+        cout<<"Codigo:";
+        cin >>p[i].codigo;
+        cout <<"Preco unitario ";
+        cin >> p[i].preco_unitario;
+    }
+}
+
+void leituraIngreduentente (struct Ingredientes ing[]) {
+    for (int i = 0; i < 3; i++) {
+        cout <<"Ingredientes: " << i+1 << ": ";
+        cout<<"Codigo: ";
+        cin >> ing[i].codigo;
+        cout <<"Descricao: ";
+        cin >> ing[i].descricao;
+        cout<<"Quantidade Estoque: ";
+        cin >>ing[i].quant_estoque;
+        cout<<"Estoque Minimo: ";
+        cin >> ing[i].estoque_minimo;
+        cout<<"Estoque Maximo: ";
+        cin>> ing[i].estoque.maximo;
+        cout<<"Preco Unitario: ";
+        cin >> ing[i].preco_unitario;
+
+    }
+}
+
+
+
 
 int main() {
 
